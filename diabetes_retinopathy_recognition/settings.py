33aeb1159eae2ffdc7c2c092afc6b5862dcf4e1a
amir78pgd <46083363+amir78pgd@users.noreply.github.com>
2019-08-31 19:17:25 +0530

    Update settings.py

--- diabetes_retinopathy_recognition/settings.py
+++ diabetes_retinopathy_recognition/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
